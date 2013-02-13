fe5522604933b1b0203b3da2d75515d927a49859
ssjuampi23 <sspablo23@hotmail.com>
2013-02-12 23:00:10 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 super_zapatos
 =============
+
+**Start date:** 2013/02/12
+
+**Collaborator:**
+* Juan Pablo Soto Github user: ssjuampi23
+
+**Repository**
+* Github url: git@github.com:ssjuampi23/super_zapatos.git
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperZapatos::Application.config.secret_token = '59625d5f9f77ea6ca974ea4afe5f03096b535bf1290e49e2a4b3807ed9a7437a60799f1830730a7102d1abb654c482e25cdd15d885d629ed8d3d1d602d6afdff'
